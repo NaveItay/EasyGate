0e67bfe89cf3bfbf1d2ede92c6b70975caf50c61
naveitayx <59213562+NaveItay@users.noreply.github.com>
2024-09-20 04:25:30 +0300

    EasyGate

---
+++ script.js
@@ -0,0 +1,4 @@
+// Basic JavaScript for potential interactions (you can expand this later)
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("EasyGate website is loaded and ready!");
+});

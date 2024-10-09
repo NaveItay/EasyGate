49bb39641278cfd6386f0a6a885a8da9b86b6546
naveitayx <59213562+NaveItay@users.noreply.github.com>
2024-10-09 23:36:50 +0300

    update header

--- script.js
+++
@@ -1,4 +0,0 @@
-// Basic JavaScript for potential interactions (you can expand this later)
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("EasyGate website is loaded and ready!");
-});

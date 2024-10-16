494d734f03f2b31c0d9f7a757f768b37d91ef2a7
naveitayx <59213562+NaveItay@users.noreply.github.com>
2024-10-17 01:14:10 +0300

    hide video when hamburger icon active

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+// main.js
+
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const mainContent = document.querySelector('.main-content');
+const header = document.querySelector('.header'); // Added this line
 const links = document.querySelectorAll('.nav-links a');
 
 function closeMenu() {
     navLinks.classList.remove('active');
     hamburger.classList.remove('active');
     mainContent.classList.remove('blur');
+    header.classList.remove('active'); // Added this line
 }
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     hamburger.classList.toggle('active');
     mainContent.classList.toggle('blur'); // Toggle blur effect on main content
+    header.classList.toggle('active'); // Added this line
 });
 
 // Close the menu when a navigation link is clicked
